99e2e205da551a57d2a3ae1c258849ec955b5e97
bishop <bishop@krouts.com>
2022-12-06 08:23:22 -0500

    html and js

---
+++ app.js
@@ -0,0 +1,13 @@
+function randommRGB() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`
+};
+
+const letter = document.querySelectorAll('.letter');
+const interValid = setInterval(function () {
+    for (let letter of letters) {
+        letter.style.color = randomRG();
+    }
+}, 1500);
